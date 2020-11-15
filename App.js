ea331353332ee4e957318db94fd79489b01f7837
alishalawani <alishalawani@gmail.com>
2020-11-14 19:41:53 -0600

    fetched the messages and set up app

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const url = 'https://message-board-db.herokuapp.com/api/messages';
+	const [messages, setMessages] = useState([]);
+	const fetchMessages = () => {
+		fetch(url)
+			.then((res) => res.json())
+			.then((res) => {
+        setMessages([...res]);
+        console.log(res)
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
+	useEffect(() => {
+    fetchMessages();
+  }, []);
+
+	return (<SafeAreaView style={styles.container}>
+    {messages ? 
+      messages.map((message)=>{
+      return <Text style={styles.text}>{message.message}</Text>
+      }) : <Text>Messages loading...</Text>
+    }
+  </SafeAreaView>)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
   },
+  text:{
+    color:'black',
+    padding:20,
+    backgroundColor:'dodgerblue',
+    marginTop: 20,
+    width:'90%',
+  }
 });
